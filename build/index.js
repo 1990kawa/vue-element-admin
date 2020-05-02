03555f98685bde642c8d864f79b172ccaaafe47c
YK <ko.with.ja@gmail.com>
2020-05-03 00:25:15 +0900

    fix

--- build/index.js
+++ build/index.js
@@ -5,33 +5,28 @@ const rawArgv = process.argv.slice(2)
 const args = rawArgv.join(' ')
 const port = process.env.PORT || 3000
 
-if (process.env.npm_config_preview || rawArgv.includes('--preview')) {
-  const report = rawArgv.includes('--report')
+const report = rawArgv.includes('--report')
 
-  sh(`vue-cli-service build ${args}`)
+const publicPath = config.publicPath
 
-  const publicPath = config.publicPath
-
-  console.log(publicPath)
-
+async function start() {
   var connect = require('connect')
   var serveStatic = require('serve-static')
   const app = connect()
-
+  
   app.use(
     publicPath,
     serveStatic('./dist', {
       index: ['index.html', '/']
     })
   )
-
+  
   app.listen(port, function () {
     console.log(chalk.green(`> Preview at  http://localhost:${port}${publicPath}`))
     if (report) {
       console.log(chalk.green(`> Report at  http://localhost:${port}${publicPath}report.html`))
     }
-
+  
   })
-} else {
-  run(`vue-cli-service build ${args}`)
 }
+start()

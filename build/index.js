f37c71cf1abe999651abb17b8c18f762add88ea2
YK <ko.with.ja@gmail.com>
2020-05-02 17:41:42 +0900

    fix

--- build/index.js
+++ build/index.js
@@ -3,13 +3,13 @@ const chalk = require('chalk')
 const config = require('../vue.config.js')
 const rawArgv = process.argv.slice(2)
 const args = rawArgv.join(' ')
+const port = process.env.PORT || 3000
 
 if (process.env.npm_config_preview || rawArgv.includes('--preview')) {
   const report = rawArgv.includes('--report')
 
   run(`vue-cli-service build ${args}`)
 
-  const port = 9526
   const publicPath = config.publicPath
 
   var connect = require('connect')
